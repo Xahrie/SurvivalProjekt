49927002cfbb3fca4ad992ca2ab211d54a753c7a
SrAkzeptiert <marioangelo06@fn.de>
2018-09-29 23:26:04 +0200

    Next Version by:
    Signed-off-by: SrAkzeptiert <marioangelo050505@gmail.com>

--- src/net/mmm/survival/player/Licence.java
+++ src/net/mmm/survival/player/Licence.java
@@ -1,7 +1,28 @@
 package net.mmm.survival.player;
 
+import net.mmm.survival.util.SurvivalWorld;
+
 public enum Licence {
-  NETHERLIZENZ,
-  ENDLIZENZ,
-  BAULIZENZ
+	
+	NETHERLIZENZ(0),
+	ENDLIZENZ(1),
+	BAULIZENZ(2);
+	
+  /*
+   *  
+   *
+   */
+  private final Integer id;
+  
+	Licence(final Integer id) {
+	  this.id = id;
+	}
+	
+	public Integer getID() {
+	  return id;
+	}
+	
+	public String getWorldName() {
+	  return id == 0 ? SurvivalWorld.NETHER.name() : id == 1 ? SurvivalWorld.END.name() : SurvivalWorld.BAUWELT.name();
+	}
 }
