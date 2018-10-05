7c05498803e467c6734f7065c69ac29414ed2006
SrAkzeptiert <marioangelo050505@gmail.com>
2018-10-05 04:25:42 +0200

    Updated: Added Level System

--- src/net/mmm/survival/player/LevelPlayer.java
+++ src/net/mmm/survival/player/LevelPlayer.java
@@ -1,5 +1,7 @@
 package net.mmm.survival.player;
 
+import net.mmm.survival.SurvivalData;
+
 /* 
  * @author Suders
  * Date: 05.10.2018
@@ -8,5 +10,43 @@ package net.mmm.survival.player;
 */
 
 public class LevelPlayer {
-
+  
+  private Float exp;
+  private Integer level;
+  
+  public LevelPlayer(final Float exp) {
+    this.exp = exp != null ? exp : 100F;//Setzt die Exp
+    float xp = 100F;
+    Integer level = 1;
+    for(float xp2 : SurvivalData.getInstance().getLevels().values()) {//Setzt das Level an Hand der Exp | NICHT Getestet
+      if(xp <= exp && xp2 > exp) {
+        this.level = level;
+        break;
+      }
+      xp = xp2;
+      level++;
+    }
+  }
+  
+  /*
+   * @return Returnt den EXP Wert, nicht sein richtiges Level.
+   * Bedeutet nicht eine Zahl zwischen 1-99 sondern eine über 100F
+   */
+  public Float getExp() {
+    return this.exp;
+  }
+  
+  public Integer getLevel() {
+    return this.level;
+  }
+  
+  /*
+   * @return Returnt ob Level dem Spieler erfolgreich hinzugefügt wurden konnte
+   * @param level Wie viel Level dem Spieler hinzugefügt werden soll
+   */
+  public boolean addExp(final Float exp) {
+    if(exp == 0F) return false;
+    this.exp += exp;
+    return true;
+  }
 }
