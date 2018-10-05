49d63f15a192b7b4d7de74f61bc94facdbb88ded
SrAkzeptiert <marioangelo050505@gmail.com>
2018-10-05 04:25:42 +0200

    Updated: Added Level System

--- src/net/mmm/survival/SurvivalData.java
+++ src/net/mmm/survival/SurvivalData.java
@@ -1,11 +1,13 @@
 package net.mmm.survival;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import net.mmm.survival.mysql.AsyncMySQL;
 import net.mmm.survival.player.SurvivalPlayer;
 import net.mmm.survival.regions.DynmapWorldGuardPlugin;
+import net.mmm.survival.util.LevelState;
 
 public class SurvivalData {
   private static SurvivalData survivalData;
@@ -13,6 +15,7 @@ public class SurvivalData {
   private final AsyncMySQL async = new AsyncMySQL();
   private final Map<UUID, String> playerCache;
   private final Map<UUID, SurvivalPlayer> players;
+  private final Map<Integer, Float> levels;
   private DynmapWorldGuardPlugin dynmap;
 
   /**
@@ -21,6 +24,13 @@ public class SurvivalData {
   public SurvivalData() {
     players = async.getPlayers(); // Lade Spieler(SurvivalPlayer) von MySQL
     playerCache = async.getPlayerCache(); // Lade Spielerdatenbank von MySQL
+    levels = new HashMap<Integer, Float>();
+    Float exp = 100F;
+    for(int i = 1; i<100;i++) {
+      final LevelState state = LevelState.LEVEL_1_BETWEEN_20.getLevelState(i);
+      exp += state.getFactor() * exp;
+      levels.put(i, exp); 
+    }
   }
 
   /**
@@ -45,7 +55,11 @@ public class SurvivalData {
   void setDynmap(final DynmapWorldGuardPlugin dynmap) {
     this.dynmap = dynmap;
   }
-
+  
+  public Map<Integer, Float> getLevels() {
+    return levels;
+  }
+  
   public Map<UUID, String> getPlayerCache() {
     return playerCache;
   }
